98d6c37ab0c5a0b777d554ab4abe0818eb862161
shreya demo <cse23.shreyajain@ggct.co.in>
2026-05-11 19:49:13 +0530

    Fix: register route to signup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -38,7 +38,7 @@ function Register() {
     try {
 
       const response = await fetch(
-        "https://securex-backend-mlm3.onrender.com/api/auth/register",
+        "https://securex-backend-mlm3.onrender.com/api/auth/signup",
         {
           method: "POST",
 
